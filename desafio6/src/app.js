a02533034dcd3cb70c73877633a45543703d78fe
agustin <agustinsanchezx@gmail.com>
2023-08-01 20:34:52 -0300

    add dotenv

--- desafio6/src/app.js
+++ desafio6/src/app.js
@@ -2,16 +2,21 @@ import express from 'express'
 import mongoose from 'mongoose'
 import productRouter from './routes/productRouter.js'
 import cartRouter from './routes/cartRouter.js'
+import * as dotenv from "dotenv";
+// Config dotenv
+dotenv.config();
 
 // Conectando a mongoose
+const URL_MONGOOSE = process.env.URL_MONGOOSE
+
 try {
-    await mongoose.connect('mongodb+srv://ecommerce-coder:1234@ecommerce-coder.xd2y4j9.mongodb.net/?retryWrites=true&w=majority')
+    await mongoose.connect(URL_MONGOOSE)
 } catch (error) {
     console.log(error)
 }
 
 // Creacion del puerto y conexion a server en express
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
